b2d03f52535d0d982f83dca4f3f9b12802ca15ab
ruin09 <meme_hm@naver.com>
2021-04-21 00:12:15 +0900

    Adjust webpack settings

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Project, ProjectStatus } from '../models/project.js';
-import { Component } from './base-component.js';
-import { autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
-import { DragTarget } from '../models/drag-drop.js';
-import { ProjectItem } from './project-item.js';
+import { Project, ProjectStatus } from '../models/project';
+import { Component } from './base-component';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
+import { DragTarget } from '../models/drag-drop';
+import { ProjectItem } from './project-item';
 
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
